b13f7acc35668dc4c4c4cfbfc4c3b559b5a7793a
n-buga <bugakova_nadya@mail.ru>
2019-12-06 15:49:32 +0300

    Final versions

--- 01_flags.cpp
+++ 01_flags.cpp
@@ -8,18 +8,23 @@ const int MAXN = 100000;
 
 pthread_mutex_t mutex;
 pthread_cond_t cond_flag_changed;
+pthread_cond_t cond_flag_changed_write;
 int flag;
 
 void* consumer(void*) {
     bool is_finished = false;
     while (!is_finished) {
+        pthread_mutex_lock(&mutex);
         while (flag == 0) {
+            pthread_cond_wait(&cond_flag_changed, &mutex);
         }
         std::cout << "Flag changed to " << flag << std::endl;
         if (flag == -1) {
             is_finished = true;
         }
         flag = 0;
+        pthread_cond_signal(&cond_flag_changed_write);
+        pthread_mutex_unlock(&mutex);
     }
     return nullptr;
 }
@@ -27,15 +32,31 @@ void* consumer(void*) {
 int main() {
     pthread_mutex_init(&mutex, nullptr);
     pthread_cond_init(&cond_flag_changed, nullptr);
+    pthread_cond_init(&cond_flag_changed_write, nullptr);
     pthread_t id;
     assert(pthread_create(&id, nullptr, consumer, nullptr) == 0);
     for (int i = 1; i <= MAXN; i++) {
+        pthread_mutex_lock(&mutex);
         flag = i;
+        pthread_cond_signal(&cond_flag_changed);
+        while (flag != 0) {
+            pthread_cond_wait(&cond_flag_changed_write, &mutex);
+        }
+        pthread_mutex_unlock(&mutex);
     }
+    pthread_mutex_lock(&mutex);
     flag = -1;
+    pthread_cond_signal(&cond_flag_changed);
+    pthread_mutex_unlock(&mutex);
     assert(pthread_join(id, nullptr) == 0);
     pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_flag_changed_write);
     pthread_cond_destroy(&cond_flag_changed);
+
     return 0;
 }
 
+// pthread_mutex_lock(&mutex);
+// pthread_mutex_unlock(&mutex);
+// pthread_cond_wait(&cond_flag_changed, &mutex);
+// pthread_cond_signal(&cond_flag_changed);
--- 02_commands.cpp
+++ 02_commands.cpp
@@ -6,6 +6,12 @@
 #include <iostream>
 
 
+int value_to_show = 1;
+pthread_mutex_t mutex;
+pthread_cond_t cond_calculate_next; // Guarded by mutex.
+bool is_finished = true;  // Guarded by mutex.
+bool is_program_finished = false;  // Guarded by mutex.
+
 int calculate_next(int value) {
     std::cout << "started" << std::endl;
     int next_value = 1;
@@ -20,14 +26,64 @@ int calculate_next(int value) {
 }
 
 
+void* calculate_next_service(void*) {
+    bool is_program_finished_snapshot = false;
+    while (!is_program_finished_snapshot) {
+        pthread_mutex_lock(&mutex);
+        if (is_program_finished) {
+            break;
+        }
+        pthread_cond_wait(&cond_calculate_next, &mutex);
+        if (is_program_finished) {
+            break;
+        }
+        int value_snapshot = value_to_show;
+        is_finished = true;
+        pthread_mutex_unlock(&mutex);
+        
+        calculate_next(value_snapshot);
+
+        pthread_mutex_lock(&mutex);
+        value_to_show = value_snapshot;
+        is_program_finished_snapshot = is_finished;
+        pthread_mutex_unlock(&mutex);
+    }
+    return nullptr;
+}
+
 int main() {
+    pthread_mutex_init(&mutex, nullptr);
+    pthread_cond_init(&cond_calculate_next, nullptr);
+    pthread_t id;    
+    assert(pthread_create(&id, nullptr, calculate_next_service, nullptr) == 0);
+
     std::string command;
     while (std::cin >> command) {
         if (command == "show") {
-            
+            pthread_mutex_lock(&mutex);
+            std::cout << "Value is " << value_to_show << std::endl;
+            pthread_mutex_unlock(&mutex);
         } else if (command == "next") {
-            
+            pthread_mutex_lock(&mutex);
+            if (is_finished) {            
+                pthread_cond_signal(&cond_calculate_next);
+                is_finished = false;
+            } else {
+                std::cout << "Still calculating" << std::endl;
+            }
+            pthread_mutex_unlock(&mutex);            
+        } else if (command == "exit") {
+            pthread_mutex_lock(&mutex);
+            is_program_finished = true;
+            pthread_cond_signal(&cond_calculate_next);
+            pthread_mutex_unlock(&mutex);
+            break;
         }
     }
+
+    assert(pthread_join(id, nullptr) == 0);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_calculate_next);
+
     return 0;
 }
